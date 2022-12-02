0039cfc1bd5d1ec098e43e3a184a0e634c5640c7
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-12-01 23:19:05 -0500

    Finished Lab

---
+++ app/src/main/java/com/maximo/mytaskmanager/AddTask.java
@@ -0,0 +1,32 @@
+package com.maximo.mytaskmanager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        Button submitTaskButton = AddTask.this.findViewById(R.id.AddTaskActivitySubmitTaskButton);
+
+        submitTaskButton.setOnClickListener(view -> {
+
+            Log.v("", "Very Verbose");
+            Log.d("", "Debug");
+            Log.i("", "Information");
+            Log.w("", "Warning");
+            Log.e("", "Error");
+            Log.wtf("", "What a terrible failure");
+            TextView greeting = AddTask.this.findViewById(R.id.AddTaskActivityTextViewSubmittedMessage);
+            greeting.setText("Submitted");
+        });
+    }
+
+}
---
+++ app/src/main/java/com/maximo/mytaskmanager/MainActivity.java
@@ -0,0 +1,38 @@
+package com.maximo.mytaskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupAddTaskButton();
+        setupAllTasksButton();
+    }
+
+
+    public void setupAddTaskButton(){
+        Button addToTaskButton = MainActivity.this.findViewById(R.id.MainActivityAddTaskButton);
+        addToTaskButton.setOnClickListener(view -> {
+            Intent goToAddTaskActivity = new Intent(this, AddTask.class);
+            startActivity(goToAddTaskActivity);
+        });
+    }
+
+    public void setupAllTasksButton(){
+        Button allTasksButton = MainActivity.this.findViewById(R.id.MainActivityAllTasksButton);
+        allTasksButton.setOnClickListener(view -> {
+            Intent goToAddTaskActivity = new Intent(this, AllTasks.class);
+            startActivity(goToAddTaskActivity);
+        });
+    }
+
+}
