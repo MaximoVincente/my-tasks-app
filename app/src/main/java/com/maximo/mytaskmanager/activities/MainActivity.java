7554c1561b7ecc4c470bf5c71f53af1dd6c12cf2
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-12-13 14:46:28 -0500

    added authO in main activity

--- app/src/main/java/com/maximo/mytaskmanager/activities/MainActivity.java
+++ app/src/main/java/com/maximo/mytaskmanager/activities/MainActivity.java
@@ -19,6 +19,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Task;
 import com.maximo.mytaskmanager.R;
@@ -29,6 +31,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String DATABASE_NAME = "myTasksAppDatabase";
+    public static final String TAG = "mainActivity";
     public static final String TASK_TITLE_TAG = "taskTitle";
     public static final String TASK_TITLE_DESCRIPTION = "taskDescription";
     public static final String TASK_TITLE_STATE = "taskState";
@@ -40,6 +43,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //AuthO Signup Setup
+        Amplify.Auth.signUp(
+            "mavincen",
+            "password",
+            AuthSignUpOptions.builder()
+            .userAttribute(AuthUserAttributeKey.preferredUsername(), "mavincen")
+            .userAttribute(AuthUserAttributeKey.email(), "maximovincente@gmail.com")
+            .userAttribute(AuthUserAttributeKey.nickname(), "max")
+            .build(),
+            success -> Log.i(TAG, "Signup Successful" + success.toString()),
+            failure -> Log.w(TAG, "Signup Failed with username " + "mavincen" + "with the message: " + failure )
+        );
+
+        //AuthO Verification
+        Amplify.Auth.confirmSignUp(
+                "maximovincente@gmail.com",
+                "123456",
+                success ->  Log.i (TAG, "Verification Successful"),
+                failure -> Log.w(TAG, "Verification Failed ")
+        );
+
+        //AuthO Login
+        Amplify.Auth.signIn(
+                "mavincen",
+                "password",
+                success -> {},
+                failure -> {}
+        );
+        //Auth0 Logout
+        Amplify.Auth.signOut(
+                "mavincen",
+                success -> Log.i (TAG, "Logout Successful"),
+                failure -> Log.w (TAG, "Logout Failed")
+        );
+
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         setupAddTaskButton();
         setupAllTasksButton();
